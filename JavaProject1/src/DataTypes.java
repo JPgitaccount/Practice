bc16faa50ce349295315b80f76f4405aa222b058
jasmi <jasmi@JASMIN-PC>
2022-07-27 16:18:50 -0600

    my first commit

---
+++ JavaProject1/src/DataTypes.java
@@ -0,0 +1,27 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		        int a=10;
+		     	double b=20.5;
+				char c='J';
+				boolean d=false;
+				String e="Welcome Jasmin";
+				
+				System.out.println(a);
+				System.out.println(b);
+				System.out.println(c);
+				System.out.println(d);
+				System.out.println(e);
+				System.out.println(e.length());
+				System.out.println(e.toUpperCase());
+				System.out.println(e.toLowerCase());
+				
+				System.out.println(e.indexOf("Jasmin"));
+				
+				
+				
+
+	}
+
+}
